e266bad08e42b3c92fbebed7e5fd2fc9d7bcd764
sirisha <103618216+sirishacyd@users.noreply.github.com>
2023-04-11 16:07:17 -0700

    Update and rename qrcode.py to qr_code_generator.py

--- qr_code_generator.py
+++ qr_code_generator.py
@@ -1,8 +1,9 @@
 import qrcode
+
 def generate_qrcode(text):
     qr = qrcode.QRCode(
-        version = 1,
-        error_correction = qrcode.constants. ERROR_CORRECT_L,
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
         box_size=10,
         border=4,
     )
